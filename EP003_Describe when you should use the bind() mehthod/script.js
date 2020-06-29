2359f59ef7d494ab8ab443156a748e529646c5fd
Stephen Printup <corky@corkysdevshop.com>
2020-06-29 10:13:09 -0600

    comments about this keyword

--- EP003_Describe when you should use the bind() mehthod/script.js
+++ EP003_Describe when you should use the bind() mehthod/script.js
@@ -1,14 +1,16 @@
+//Below the 'this' keyword on line 6 gets reassigned from 'Ray' on line 4 to 'Bob' online 12. 
+
 var myModule = {
     name: 'Ray',
     getName: function() {
-        return console.log(this.name); // this here in normal context means myModule
+        return console.log(this.name); // this here in normal context means myModule.name = 'Ray'
     }
 };
 
-myModule.getName();
+myModule.getName(); // Ray
 
-var someName = myModule.getName.bind({name:'Bob'}); //calling method in module
-someName(); 
+var someName = myModule.getName.bind({name:'Bob'}); //calling getName method in module, but binding 'this' keyword to a new object with a property 'name' that equals 'Bob'
+someName(); // Bob
 /*
     Ray
     Bob
