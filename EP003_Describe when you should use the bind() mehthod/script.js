b8bda04a9f967106f9e8b171ecf80244fe1e7521
Stephen Printup <corky@corkysdevshop.com>
2020-06-29 10:08:51 -0600

    additional set up

--- EP003_Describe when you should use the bind() mehthod/script.js
+++ EP003_Describe when you should use the bind() mehthod/script.js
@@ -1,11 +1,15 @@
 var myModule = {
     name: 'Ray',
     getName: function() {
-        return console.log(this.name);
+        return console.log(this.name); // this here in normal context means myModule
     }
 };
 
 myModule.getName();
 
-var someName = myModule.getName.bind
-someName();
+var someName = myModule.getName.bind({name:'Bob'}); //calling method in module
+someName(); 
+/*
+    Ray
+    Bob
+*/
